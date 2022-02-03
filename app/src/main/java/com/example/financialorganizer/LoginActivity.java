70ee4ad6a0efce931c65fbd7653c6682c09151c0
Ben Weber <weberbencs@gmail.com>
2022-02-03 14:56:37 -0600

    2/3 groundwork (#1)
    * Launch Activity
    
    * Registration page layout
    
    * Connecting with Firebase, rebuild gradle
    
    * User Auth through firebase

---
+++ app/src/main/java/com/example/financialorganizer/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.example.financialorganizer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button loginBtn;
+    private TextView signUp;
+
+    private FirebaseAuth fAuth;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+        signUp = findViewById(R.id.signUp);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, UserRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailInput = email.getText().toString();
+                String passwordInput = password.getText().toString();
+
+                if(TextUtils.isEmpty(emailInput)){
+                    email.setError("Email is required");
+                }
+                if(TextUtils.isEmpty(passwordInput)){
+                    password.setError("Password is required");
+                } else {
+                    progressDialog.setMessage("Login in progress");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+
+                    fAuth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                progressDialog.dismiss();
+                            } else{
+                                Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}

08759ec9bc34e23a81297d7193806c0f68a1dd40
benweberswe@gmail.com <benweberswe@gmail.com>
2022-02-04 14:47:40 -0600

    budget totaling

--- app/src/main/java/com/example/financialorganizer/Budget.java
+++ app/src/main/java/com/example/financialorganizer/Budget.java
@@ -26,8 +26,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.joda.time.DateTime;
 import org.joda.time.Months;
@@ -65,6 +68,25 @@ public class Budget extends AppCompatActivity {
         linearLayoutManager.setReverseLayout(true);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
+
+        budgetRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int totalAmount = 0;
+
+                for(DataSnapshot snap : snapshot.getChildren()){
+                    Data data = snap.getValue(Data.class);
+                    totalAmount += data.getAmount();
+                    String sTotal = String.valueOf("Monthly Budget: $" + totalAmount);
+                    totalBudgetView.setText(sTotal);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         fab = findViewById(R.id.fab);
 

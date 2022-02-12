5ab2081c4c2fd4a9124e9c35e00798b9f1c17d85
benweberswe@gmail.com <benweberswe@gmail.com>
2022-02-12 17:03:25 -0600

    More analytics design view, activities

---
+++ app/src/main/java/com/example/financialorganizer/ActivityWeeklyAnalytics.java
@@ -0,0 +1,14 @@
+package com.example.financialorganizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityWeeklyAnalytics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_analytics);
+    }
+}
--- app/src/main/java/com/example/financialorganizer/AnalyticsChoiceActivity.java
+++ app/src/main/java/com/example/financialorganizer/AnalyticsChoiceActivity.java
@@ -1,14 +1,46 @@
 package com.example.financialorganizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AnalyticsChoiceActivity extends AppCompatActivity {
+    private CardView todaysCardView, weeksCardView, monthsCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analytics_choice);
+
+        todaysCardView = findViewById(R.id.todaysCardView);
+        weeksCardView = findViewById(R.id.weeksCardView);
+        monthsCardView = findViewById(R.id.monthsCardView);
+
+        todaysCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AnalyticsChoiceActivity.this, DailyAnalyticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weeksCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AnalyticsChoiceActivity.this, ActivityWeeklyAnalytics.class);
+                startActivity(intent);
+            }
+        });
+
+        monthsCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AnalyticsChoiceActivity.this, ActivityMonthlyAnalytics.class);
+                startActivity(intent);
+            }
+        });
     }
 }
